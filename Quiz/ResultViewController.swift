7b909088d253aaeb404d60ca35a45c83c62d097c
hmlab book pro <saki.tommy69@gmail.com>
2016-02-27 17:15:52 +0900

    変更しました

--- Quiz/ResultViewController.swift
+++ Quiz/ResultViewController.swift
@@ -16,7 +16,7 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        resultLabel.text = "\(correctAnswer)"
+        resultLabel.text = "30問中\(correctAnswer)問でした！"
     }
 
     override func didReceiveMemoryWarning() {
