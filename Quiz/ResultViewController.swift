2f60438bcac205a7006071304f6abaea14e175cc
hmlab book pro <saki.tommy69@gmail.com>
2016-02-29 18:03:20 +0900

    変更しました

--- Quiz/ResultViewController.swift
+++ Quiz/ResultViewController.swift
@@ -1,27 +1,24 @@
 //
 //  ResultViewController.swift
 //  Quiz
-//
-//  Created by ohtatomotaka on 2015/02/10.
-//  Copyright (c) 2015年 LifeisTech. All rights reserved.
-//
+
 
 import UIKit
 
 class ResultViewController: UIViewController {
     var correctAnswer:Int = 0
     @IBOutlet var resultLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         resultLabel.text = "30問中\(correctAnswer)問でした！"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
